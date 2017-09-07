a510d2b28100d20aed51b18828e567e00f357c7d
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-09-07 20:52:10 +0200

    Avoid dummy.

--- frag6_overhead.py
+++ frag6_overhead.py
@@ -13,15 +13,13 @@ from scapy.all import *
 pid=os.getpid()
 eid=pid & 0xffff
 payload="ABCDEFGHIJKLMNOP"
-dummy="01234567"
 packet=IPv6(src=LOCAL_ADDR6, dst=REMOTE_ADDR6)/ \
     ICMPv6EchoRequest(id=eid, data=payload)
 frag=[]
 fid=pid & 0xffffffff
 frag.append(IPv6ExtHdrFragment(nh=58, id=fid, m=1)/str(packet)[40:48])
 frag.append(IPv6ExtHdrFragment(nh=58, id=fid, offset=1, m=1)/str(packet)[48:56])
-frag.append(IPv6ExtHdrFragment(nh=58, id=fid, offset=1)/
-    (dummy+str(packet)[56:64]))
+frag.append(IPv6ExtHdrFragment(nh=58, id=fid, offset=1)/str(packet)[48:64])
 eth=[]
 for f in frag:
 	pkt=IPv6(src=LOCAL_ADDR6, dst=REMOTE_ADDR6)/f
