b1286eb7cc46d111fc3326086446c07d03cd62ae
Alexander Bluhm <alexander.bluhm@gmx.net>
2017-09-07 20:54:04 +0200

    Sync to frag4.  Avoid dummy.

--- frag6_overtail.py
+++ frag6_overtail.py
@@ -13,13 +13,12 @@ from scapy.all import *
 pid=os.getpid()
 eid=pid & 0xffff
 payload="ABCDEFGHIJKLMNOP"
-dummy="01234567"
 packet=IPv6(src=LOCAL_ADDR6, dst=REMOTE_ADDR6)/ \
     ICMPv6EchoRequest(id=eid, data=payload)
 frag=[]
 fid=pid & 0xffffffff
 frag.append(IPv6ExtHdrFragment(nh=58, id=fid, offset=2)/str(packet)[56:64])
-frag.append(IPv6ExtHdrFragment(nh=58, id=fid, offset=1)/dummy)
+frag.append(IPv6ExtHdrFragment(nh=58, id=fid, offset=1, m=1)/str(packet)[48:56])
 frag.append(IPv6ExtHdrFragment(nh=58, id=fid, m=1)/str(packet)[40:56])
 eth=[]
 for f in frag:
